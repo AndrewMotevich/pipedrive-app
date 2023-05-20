e2fab22e3edde6b936db7667d92f2b78085e7b00
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-20 20:21:02 +0300

    feat: check get deals

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,17 +3,17 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Main = () => {
-  const [deals, setDeals] = useState<{ title: string }[]>([]);
-  useEffect(() => {
-    (async () => {
-      await axios
-        .get<object[]>("https://pipedrive-app.vercel.app/api/deals")
-        .then((res) => {
-          return setDeals(res as unknown as { title: string }[]);
-        });
-    })();
-  }, []);
-  return <div className="App">{deals[0].title}</div>;
+  // const [deals, setDeals] = useState<{ title: string }[]>([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     await axios
+  //       .get<object[]>("https://pipedrive-app.vercel.app/api/deals")
+  //       .then((res) => {
+  //         return setDeals(res as unknown as { title: string }[]);
+  //       });
+  //   })();
+  // }, []);
+  return <div className="App"></div>;
 };
 
 export default Main;
