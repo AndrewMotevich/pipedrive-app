cedeed7fff387a76b06c5fdea442dfae4de0ea1f
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-20 17:35:09 +0300

    feat: add callback function

---
+++ api/callback.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import pipedrive from "pipedrive";
+
+const apiClient = new pipedrive.ApiClient();
+const api = new pipedrive.DealsApi(apiClient);
+let TOKENS = {};
+
+export default async function callback(req: Request, res: Response) {
+  const code = req.query.code;
+  const tokenPromise = apiClient.authorize(code);
+
+  tokenPromise.then((res) => (TOKENS = res));
+  res.json(TOKENS);
+}
