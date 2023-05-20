7b37ba6a827405f19e8ce46d8e3a58760637daaa
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-20 18:15:12 +0300

    feat: add callback function

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import { error } from "node:console";
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
 import { redirect } from "react-router-dom";
+// @ts-ignore
 import pipedrive from "pipedrive";
 
 // Constants
@@ -16,7 +17,7 @@ let TOKENS = {};
 let oauth2 = apiClient.authentications.oauth2;
 oauth2.clientId = "0494d1b7cd096deb";
 oauth2.clientSecret = "cf32220b35508cce863201560369ed77046a1356";
-oauth2.redirectUri = "https://pipedrive-app.vercel.app/callback";
+oauth2.redirectUri = "https://pipedrive-app.vercel.app/api/callback";
 
 // Create http server
 const app = express();
@@ -59,7 +60,7 @@ app.get("/deals", async (req, res) => {
 
 app.get("/callback", (req, res) => {
   const code = req.query.code;
-  const tokenPromise = apiClient.authorize(code);
+  const tokenPromise = apiClient.authorize(code) as Promise<object>;
 
   // add access token to session
   req.session.accessToken = apiClient.authentications.oauth2.accessToken;
@@ -69,7 +70,7 @@ app.get("/callback", (req, res) => {
 });
 
 app.get("/refresh", (req, res) => {
-  const refreshPromise = apiClient.refreshToken();
+  const refreshPromise = apiClient.refreshToken() as Promise<object>;
   refreshPromise.then(
     (res) => {
       TOKENS = res;
