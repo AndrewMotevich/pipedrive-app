1bf12f546f98a9a716bcb1e880a2b2337fe960f0
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-20 10:28:27 +0300

    feat: try to use custom serverless functions

--- api/handler.ts
+++ api/handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 export default async function handler(request: Request, response: Response) {
+  response.setHeader("Set-Cookie", "querty=12345678");
   return response.json({
     success: true,
   });
