ba64c8b2952a969739c23bd071e4c913a73763f1
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-20 17:43:40 +0300

    feat: add callback function

--- api/callback.ts
+++ api/callback.ts
@@ -5,6 +5,11 @@ const apiClient = new pipedrive.ApiClient();
 const api = new pipedrive.DealsApi(apiClient);
 let TOKENS = {};
 
+let oauth2 = apiClient.authentications.oauth2;
+oauth2.clientId = "0494d1b7cd096deb";
+oauth2.clientSecret = "cf32220b35508cce863201560369ed77046a1356";
+oauth2.redirectUri = "https://pipedrive-app.vercel.app/api/callback";
+
 export default async function callback(req: Request, res: Response) {
   const code = req.query.code;
   const tokenPromise = apiClient.authorize(code);
